17e641b0408578c9a86bea044169cd657e7b7050
Petar <blerinaazis@gmail.com>
2022-11-01 21:17:18 +0100

    domasno

---
+++ src/domasno1/Domasno1.java
@@ -0,0 +1,10 @@
+package domasno1;
+
+public class Domasno1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World OOP FIKT");
+	}
+
+}
